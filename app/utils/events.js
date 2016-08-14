6373e7b8ff1fd364216bed11aed10034431815b0
xuxiaomeng <xuxiaomeng@baozou.com>
2016-08-14 16:55:34 +0800

    feature. add select mode freamwork.

--- app/utils/events.js
+++ app/utils/events.js
@@ -1,3 +1,13 @@
 const EventEmitter = require('events');
 
-export default class ExEventEmitter extends EventEmitter {}
+export default class ExEventEmitter extends EventEmitter {
+  log(funcName, message) {
+    console.log(`[EventEmitter][${funcName}]: ${message}`)
+  }
+
+  on(eventName, listener) {
+    super.on(eventName, listener);
+    this.log('on', eventName);
+  }
+
+}
--- app/workspace/workspace.jsx
+++ app/workspace/workspace.jsx
@@ -1,4 +1,5 @@
-import AttributesBar from './attributesBar/attributesBar'
+import AttributesBar from './selectMode/attributesBar/attributesBar'
+import PatternBar from './selectMode/patternBar/patternBar'
 import Canvas from './canvas/canvas'
 
 let React = require('react');
@@ -8,17 +9,28 @@ require('./workspace.scss');
 export default class Workspace extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      mode: '',
+      exMode: ''
+    }
   }
   componentDidMount() {
+    exEventEmitter.on('modeChange', (mode) => {
+      console.log(mode);
+      this.setState({
+        mode: mode
+      })
+    })
   }
   componentWillUnmount(){
   }
   render() {
+    let classes = `mode-${this.state.mode} exmode-${this.state.exMode}`;
     return (
-      <div id="workspace">
+      <div id="workspace" className={classes}>
         <Canvas />
         <AttributesBar />
+        <PatternBar />
       </div>
     );
   }
