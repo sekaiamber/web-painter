7bdd519598452fe541e92a25f6773ad74004c558
xuxiaomeng <xuxiaomeng@baozou.com>
2016-08-15 23:14:22 +0800

    feature. add html template system.

--- app/workspace/canvas/pieces/basePiece.jsx
+++ app/workspace/canvas/pieces/basePiece.jsx
@@ -30,14 +30,20 @@ export default class BasePiece extends React.Component{
     let piece = pageEditor.getPiece(this.tag);
     piece.addPattern(pattern, index);
   }
-  updateHeight(height) {
+  updateHeight(height, patterns) {
+    if (typeof height == 'object') {
+      patterns = height;
+      height = null;
+    }
     height = height || this.state.height;
+    patterns = patterns || this.state.patterns;
     this.setState({
+      patterns: patterns,
       height: height
     })
   }
   render() {
-    let classes = "piece " + this.tag + (this.props.active ? " active" : "");
+    let classes = "piece " + this.tag + (this.props.active ? " active" : "") + (this.state.patterns.length == 0 ? " empty" : "");
     let pieceStyle = {
       height: this.state.height * window._zoom_
     }
@@ -45,9 +51,12 @@ export default class BasePiece extends React.Component{
       <div className={classes} style={pieceStyle}>
         {this.state.patterns.length == 0 ?
           <div className="empty" onClick={() => this.handleChangePatternBarState(0)}></div> :
-          this.state.patterns.map((p, i) => 
-            <div className="pattern"></div>
-          )
+          this.state.patterns.map((p, i) => {
+            let patternStyle = {
+              height: p.height * window._zoom_
+            }
+            return <div className="pattern" key={i} style={patternStyle}></div>
+          })
         }
       </div>
     );
