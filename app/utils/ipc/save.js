3faad8e023319237584e662d3c2327fd552e4d90
xuxiaomeng <xuxiaomeng@baozou.com>
2016-09-12 22:54:29 +0800

    bugfix. save project compress file fail.

--- app/utils/ipc/save.js
+++ app/utils/ipc/save.js
@@ -1,16 +1,17 @@
 const ipc = require('electron').ipcRenderer
 const nativeImage = require('electron').nativeImage
-var AdmZip = require('adm-zip');
+const archiver = require('archiver');
+const fs = require('fs');
 import fshelper from './../fs'
 import $ from 'jquery';
 
 // zip
 function createZipfile(files) {
-  var zip = new AdmZip();
+  var zip = archiver.create('zip', {});
   for (var i = 0; i < files.length; i++) {
-    var file = files[i];
-    zip.addFile(file.name, file.data)
-  };
+    var file = files[i]
+    zip.append(file.data, {'name': file.name});
+  }
   return zip;
 }
 
@@ -45,12 +46,13 @@ ipc.on('save-project', function (event, filename) {
   })
   files = files.concat(assets);
 
-  let zip = createZipfile(files);
-  // let zip = createZipfile({
-  //   name: 'assets/aaa.jpg',
-  //   data: nativeImage.createFromPath('/Volumes/disk02/web-painter/assets/images/sekai.jpg').toPNG()
-  // })
+  var output = fs.createWriteStream(filename);
+  output.on('close', function() {
+    console.log(`[save project]: save project to ${filename}`)
+  });
 
-  zip.writeZip(filename);
-  console.log(`[save project]: save project to ${filename}`)
+  let zipArchiver = createZipfile(files);
+  zipArchiver.pipe(output);
+
+  zipArchiver.finalize();
 })
